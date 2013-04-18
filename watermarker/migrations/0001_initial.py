229bf9989b3a36554c3a91146d1c4c2718d178c9
Matthew Jacobi <matt@oppian.com>
2013-04-18 18:56:39 +0100

    Adding migrations

---
+++ watermarker/migrations/0001_initial.py
@@ -0,0 +1,43 @@
+# -*- coding: utf-8 -*-
+import datetime
+from south.db import db
+from south.v2 import SchemaMigration
+from django.db import models
+
+
+class Migration(SchemaMigration):
+
+    def forwards(self, orm):
+        # Deleting model 'Watermark'
+        db.delete_table(u'watermarker_watermark')
+
+        # Adding model 'Watermark'
+        db.create_table(u'watermarker_watermark', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=50, db_index=True)),
+            ('image', self.gf('django.db.models.fields.files.ImageField')(max_length=100)),
+            ('is_active', self.gf('django.db.models.fields.BooleanField')(default=True)),
+            ('date_created', self.gf('django.db.models.fields.DateTimeField')(auto_now_add=True, blank=True)),
+            ('date_updated', self.gf('django.db.models.fields.DateTimeField')(auto_now=True, blank=True)),
+        ))
+        db.send_create_signal(u'watermarker', ['Watermark'])
+
+
+    def backwards(self, orm):
+        # Deleting model 'Watermark'
+        db.delete_table(u'watermarker_watermark')
+
+
+    models = {
+        u'watermarker.watermark': {
+            'Meta': {'ordering': "['name']", 'object_name': 'Watermark'},
+            'date_created': ('django.db.models.fields.DateTimeField', [], {'auto_now_add': 'True', 'blank': 'True'}),
+            'date_updated': ('django.db.models.fields.DateTimeField', [], {'auto_now': 'True', 'blank': 'True'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'image': ('django.db.models.fields.files.ImageField', [], {'max_length': '100'}),
+            'is_active': ('django.db.models.fields.BooleanField', [], {'default': 'True'}),
+            'name': ('django.db.models.fields.CharField', [], {'max_length': '50', 'db_index': 'True'})
+        }
+    }
+
+    complete_apps = ['watermarker']
